e982bbd6fd9b706d35f530cc9e1a7b64f18a11ca
tubi55 <tubi55@nate.com>
2021-07-17 13:52:48 +0900

    마우스호버시 자동롤링 기능 정지, 재활성화

--- js/custom.js
+++ js/custom.js
@@ -4,17 +4,28 @@ var interval = 20;
 var num = 0;
 var wid = $list_li.width();
 var len = $list_li.length;
-
+var timer;
 $list.width(len*wid);
 
-//0.02초 간격으로 ul의 marginLeft값이 2px씩 계속 줄어듬
-setInterval(function(){
+//브라우저 로딩시 배너 자동롤링 시작
+timer = setInterval(move, interval);
+
+//리스트에 마우스호버시 자동롤링 정지
+$list.on("mouseenter", function(){
+    clearInterval(timer);
+});
+
+//리스트에서 마우스가 떠나면 다시 자동롤링 시작
+$list.on("mouseleave", function(){
+    timer = setInterval(move, interval);
+});
+
+function move(){
     if(num < -wid){
         num = 0;
         $list.find("li").first().appendTo($list);
     }else{
         num-=2;
     } 
-
     $list.css({ marginLeft : num });
-},interval);
+}
