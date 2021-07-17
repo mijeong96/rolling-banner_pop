399c9d0575facfdd6fde2043fd50e7ac216db85d
tubi55 <tubi55@nate.com>
2021-07-17 13:38:55 +0900

    리스트 동작오류 수정 및 ul초기설정 자동화

--- js/custom.js
+++ js/custom.js
@@ -1,25 +1,20 @@
-/*
-특정구문을 반복처리 하는 내장함수
-setInterval(반복실행할 함수, 실행간격);
-
-setInterval(function(){
-    //2초 간격으로 해당 블록안의 구문이 반복 실행
-}, 2000);
-*/
-
 var $list = $(".list");
 var $list_li = $list.find("li");
+var interval = 20;
 var num = 0;
+var wid = $list_li.width();
+var len = $list_li.length;
 
-//0.2초 간격으로 ul의 marginLeft값이 2px씩 계속 줄어듬
+$list.width(len*wid);
+
+//0.02초 간격으로 ul의 marginLeft값이 2px씩 계속 줄어듬
 setInterval(function(){
-
-    if(num < -240){
+    if(num < -wid){
         num = 0;
-        $list_li.first().appendTo($list);
+        $list.find("li").first().appendTo($list);
     }else{
         num-=2;
     } 
-       
+
     $list.css({ marginLeft : num });
-},20);
+},interval);
