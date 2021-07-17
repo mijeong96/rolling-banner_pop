67d699e145c3d2b3b65bcc9827fb5cd2a4b2cd91
tubi55 <tubi55@nate.com>
2021-07-17 13:12:40 +0900

    setInterval을 활용해 왼쪽으로 이동하는 모션 구현

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+/*
+특정구문을 반복처리 하는 내장함수
+setInterval(반복실행할 함수, 실행간격);
+
+setInterval(function(){
+    //2초 간격으로 해당 블록안의 구문이 반복 실행
+}, 2000);
+*/
+
+var $list = $(".list");
+var $list_li = $list.find("li");
+var num = 0;
+
+//0.2초 간격으로 ul의 marginLeft값이 2px씩 계속 줄어듬
+setInterval(function(){
+    num-=2;
+    $list.css({ marginLeft : num });
+},20);
