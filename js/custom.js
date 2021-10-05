12dd5f7c98be619e380d202b5534b908c2eb2c56
mijeong96 <alwjdekdma2@gmail.com>
2021-10-05 16:25:22 +0900

    반복실행할 공통구문 외부 함수로 분리

--- js/custom.js
+++ js/custom.js
@@ -2,41 +2,25 @@
 var num=0;
 var timer;
 
-//0.05초 간격으로 반복하면서
- timer = setInterval(function(){
+ timer = setInterval(move, 50);
+
+$(".listBox").on("mouseenter",function(){
+    clearInterval(timer);
+});
+
+$(".listBox").on("mouseleave",function(){
+    timer = setInterval(move,50)
+});
+
+//setInterval로 반복할 공통 함수 분리
+function move(){
     if(num <= -120){
         num=0;
         $(".list").find("li").first().appendTo($(".list"));
     }else{
-        
         num-=2;
     }
-
-    $(".list").css({left:num})
-},50);
-
-//listbox영역에 마우스 포인터가 들어가면
-$(".listBox").on("mouseenter",function(){
-    //롤링 정지
-    clearInterval(timer);
-});
-
-//listbox영역에 마우스 포인터가 떠나면
-$(".listBox").on("mouseleave",function(){
-    //다시 롤링 시작
-    timer = setInterval(function(){
-        if(num <= -120){
-            num=0;
-            $(".list").find("li").first().appendTo($(".list"));
-        }else{
-            
-            num-=2;
-        }
-    
-        $(".list").css({left:num});
-    },50);
-});
-
-
+    $(".list").css({left:num});
+}
 
 
