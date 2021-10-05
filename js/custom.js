ae46fd862fce804e4960ef7a436f008b8cc36601
mijeong96 <alwjdekdma2@gmail.com>
2021-10-05 16:47:20 +0900

    UI디자인 적용

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,18 @@
-//처음 list의 위치값
 var num=0;
 var timer;
 
- timer = setInterval(move, 50);
+ timer = setInterval(move, 20);
 
 $(".listBox").on("mouseenter",function(){
     clearInterval(timer);
 });
 
 $(".listBox").on("mouseleave",function(){
-    timer = setInterval(move,50)
+    timer = setInterval(move,20);
 });
 
-//setInterval로 반복할 공통 함수 분리
 function move(){
-    if(num <= -120){
+    if(num <= -240){
         num=0;
         $(".list").find("li").first().appendTo($(".list"));
     }else{
